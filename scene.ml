e2b5fc7ad6f28642156b594e8b9af1729428b48b
Daniel Jann <dj293@cornell.edu>
2021-03-26 17:57:23 -0400

    started scene but idk what im doing

---
+++ scene.ml
@@ -0,0 +1,12 @@
+type player = {location: int*int}
+
+let player_loc player= player.location
+
+let move player dir= 
+  curr= player_loc player
+  match dir with
+  | "w" -> {(fst curr, snd curr + 1)}
+  | "a" -> {(fst curr - 1, snd curr)}
+  | "s" -> {(fst curr, snd curr - 1)}
+  | "d" -> {(fst curr + 1, snd curr)}
+  | _ -> player
