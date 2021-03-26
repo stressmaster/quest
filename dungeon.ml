cfc44bedaa2ca83208cb964aba8be68a6ff528dd
John Lo <jdl283@cornell.edu>
2021-03-26 17:52:21 -0400

    Edited dunegon.dungeon to have start and exit fields

--- dungeon.ml
+++ dungeon.ml
@@ -1,12 +1,16 @@
 type tile_sprite = string
 type tile = {sprite : tile_sprite; is_wall : bool}
-type dungeon = tile list list 
+type dungeon = {tiles : tile list list; start : tile; exit : tile} 
 let create_4x4_room = 
   let wall = {sprite = "wall.jpg"; is_wall = true;} in
   let path = {sprite = "path.jpg"; is_wall = false;} in 
+  let start = {sprite = "start.jpg"; is_wall = false;} in 
+  let exit = {sprite = "exit.jpg"; is_wall = false;} in 
   let layer1 = [wall; wall; wall; wall] in 
-  let layer2 = [wall; path; path; wall] in
-  [layer1; layer2; layer2; layer1]
+  let layer2 = [wall; start; path; wall] in
+  let layer3 = [wall; path; path; wall] in
+  let layer4 = [wall; path; exit; wall] in
+  { tiles = [layer1; layer2; layer3; layer4]; start = start; exit = exit}
 
 
 let rec print_dungeon_row acc row =
