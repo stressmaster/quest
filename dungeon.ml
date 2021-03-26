96c1d2fc0e72cf058ef13aa810c447e8e4e5d57c
Matthew Rosen <mhr79@cornell.edu>
2021-03-26 17:31:55 -0400

    implemented print dungeon

--- dungeon.ml
+++ dungeon.ml
@@ -7,3 +7,17 @@ let create_4x4_room =
   let layer1 = [wall; wall; wall; wall] in 
   let layer2 = [wall; path; path; wall] in
   [layer1; layer2; layer2; layer1]
+
+
+let rec print_dungeon_row acc row =
+  match row with
+| [] -> acc
+| {sprite; is_wall} :: t -> if is_wall then
+  print_dungeon_row (acc ^ "#") t else
+    print_dungeon_row (acc ^ ".") t
+
+let rec print_dungeon dungeon = 
+match dungeon with
+| [] -> ()
+| x :: x' -> print_string (print_dungeon_row "" x); print_newline(); print_dungeon x'
+
