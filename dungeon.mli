282ed5a8dbfb1a6f1a381a85ec73db97e251a86e
John Lo <jdl283@cornell.edu>
2021-03-26 23:11:42 -0400

    updated dungeon.mli

--- dungeon.mli
+++ dungeon.mli
@@ -1,3 +1,19 @@
 type tile_sprite = string
-type tile = {sprite : tile_sprite; is_wall : bool}
-type dungeon = tile list list 
+
+type color = | Green | Gray
+
+type material = 
+| Color of color
+| Sprite of tile_sprite
+
+type tile = {sprite : tile_sprite; material : material; is_wall : bool}
+
+type cell = {tile: tile; x : int; y : int}
+
+type dungeon = {
+  cells : (int * int, cell) Hashtbl.t;
+  start : (int * int); 
+  dimensions : (int * int);
+}
+
+val instantiate_dungeon : int -> int -> dungeon
