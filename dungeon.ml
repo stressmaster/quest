136d65f16ce8c2f17374c7a652bc13cad5b87d37
John Lo <jdl283@cornell.edu>
2021-03-26 21:30:58 -0400

    - created new type material for graphics - neatened up dungeon creation code

--- dungeon.ml
+++ dungeon.ml
@@ -1,34 +1,39 @@
 type tile_sprite = string
-type tile = {sprite : tile_sprite; is_wall : bool}
+type color = | Green | Gray
+type material = 
+| Color of color
+| Sprite of tile_sprite
+
+type tile = {material : material; is_wall : bool}
 
 type cell = {tile: tile; x : int; y : int}
 
 type dungeon = {
   cells : (int * int, cell) Hashtbl.t;
   start : (int * int); 
+  dimensions : (int * int);
 }
-let spawn_dungeon x y dungeon_cells= 
 
-  for y = 0 to counter_y do
-    for x = 0 to counter_x do
-      if y = 0 || y = counter_y or x =0 or x = counter_x then
-        Hashtbl.add dungeon_cells (counter_x,counter_y) {tile = {sprite = "wall.jpg"; is_wall = true}; x=counter_x; y=counter_y};
+let instantiate_dungeon_cells x y dungeon_cells= 
+  for counter_y = 0 to y do
+    for counter_x = 0 to x do
+      if counter_y = 0 || counter_y = y || counter_x =0 || counter_x = x then
+        Hashtbl.add dungeon_cells (counter_x,counter_y) {tile = {material = Sprite "wall.jpg"; is_wall = true}; x =counter_x; y=counter_y}
       else
-        Hashtbl.add dungeon_cells (counter_x,counter_y) {tile = {sprite = "path.jpg"; is_wall = false}; x=counter_x; y=counter_y};
+        Hashtbl.add dungeon_cells (counter_x,counter_y) {tile = {material = Sprite "path.jpg"; is_wall = false}; x=counter_x; y=counter_y}
       done
     done
 
-let dungeon_4x4 = {cells = spawn_dungeon 4 4 (Hashtbl.create (16)); start = (0,0)}
-
-let print_dungeon counter_x counter_y dungeon= 
-  for y = 0 to counter_y do
-    for x = 0 to counter_x do
-      let c = if (Hashtbl.find dungeon.cells (x, y).tile.is_wall)
-        then "#"
+let print_dungeon dungeon = 
+  for y = 0 to dungeon.dimensions |> fst do
+    if y > 0 then
+    print_newline()
+    else ();
+    for x = 0 to dungeon.dimensions |> snd do
+      if ((Hashtbl.find dungeon.cells (x, y)).tile.is_wall)
+        then print_string("#")
       else
-        "."
-    in
-    print_string(c);
+        print_string(".")
+    done
   done
-      print_newline();
-done
+
