1b88e90a827be2d4e381f219a4067d44d61b35a1
Daniel Jann <dj293@cornell.edu>
2021-03-27 00:21:20 -0400

    guys is this red please help me

--- state.ml
+++ state.ml
@@ -1,16 +1,18 @@
-type current = {location: int*int; room= Dungeon.dungeon; visible= int*int list}
+type current = {location: int*int; room= Dungeon.t; visible= int*int list}
 
 let init_state dungeon= 
-  let current_loc = dungeon.start_room in
+  let current_loc = dungeon.start in
   let x= fst current_loc in
   let y = snd current_loc in
   let sight= [(x-2, y+2); (x-1, y+2); (x, y+2); (x+1, y+2); (x+2, y+2);
   (x-2, y+1); (x-1, y+1); (x, y+1); (x+1, y+1); (x+2, y+1); (x-2, y);
   (x-1, y); (x, y); (x+1, y); (x+2, y); (x-2, y-1); (x-1, y-1); (x, y-1);
   (x+1, y-1); (x+2, y-1); (x-2, y-2); (x-1, y-2); (x, y-2); (x+1, y-2); (x+2, y-2)]
-  {location= dungeon.start_room; room= dungeon; visible=sight}
+  {location= current_loc; room= dungeon; visible=sight}
 
 let player_loc state= state.location
+
+let visible_tiles state= state.visible
 
 let move coor dir= 
   match dir with
