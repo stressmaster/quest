02420dd481cbc19cce41dc589e21aa6a12d55595
Matthew Rosen <mhr79@cornell.edu>
2021-03-27 01:12:41 -0400

    fixed packages

--- main.ml
+++ main.ml
@@ -1,9 +1,13 @@
 (* main *)
-open GMain
+open GtkMain
 open GdkKeysyms
 
 let locale = GtkMain.Main.init ()
+
 let main () =
-  let window = GWindow.window ~width:320 ~height:240 ~title:"Simple lablgtk program" () in
+  let window =
+    GWindow.window ~width:320 ~height:240
+      ~title:"Simple lablgtk program" ()
+  in
   let vbox = GPack.vbox ~packing:window#add () in
-  window#connect#destroy ~callback:Main.quit;
+  window#connect#destroy ~callback:Main.quit
