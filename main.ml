973439cf4922171b81d9f6f6db300ae212a37a3b
Xiaolong Gao <xg54@cornell.edu>
2021-03-26 23:56:37 -0400

    fuck9nguiwbduoeaw

--- main.ml
+++ main.ml
@@ -4,4 +4,6 @@ open GdkKeysyms
 
 let locale = GtkMain.Main.init ()
 let main () =
-  let window = GWindow.window ~width:320 ~height:240 ~title:"Fuck" ()
+  let window = GWindow.window ~width:320 ~height:240 ~title:"Simple lablgtk program" () in
+  let vbox = GPack.vbox ~packing:window#add () in
+  window#connect#destroy ~callback:Main.quit;
