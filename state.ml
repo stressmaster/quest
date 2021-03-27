05633793ec7d7699fbc2a5ce360cec898d5a6a39
Daniel Jann <dj293@cornell.edu>
2021-03-27 00:18:04 -0400

    test

--- state.ml
+++ state.ml
@@ -2,16 +2,20 @@ type current = {location: int*int; room= Dungeon.dungeon; visible= int*int list}
 
 let init_state dungeon= 
   let current_loc = dungeon.start_room in
-  let sight= []
-  {location= dungeon.start_room; room= dungeon; }
+  let x= fst current_loc in
+  let y = snd current_loc in
+  let sight= [(x-2, y+2); (x-1, y+2); (x, y+2); (x+1, y+2); (x+2, y+2);
+  (x-2, y+1); (x-1, y+1); (x, y+1); (x+1, y+1); (x+2, y+1); (x-2, y);
+  (x-1, y); (x, y); (x+1, y); (x+2, y); (x-2, y-1); (x-1, y-1); (x, y-1);
+  (x+1, y-1); (x+2, y-1); (x-2, y-2); (x-1, y-2); (x, y-2); (x+1, y-2); (x+2, y-2)]
+  {location= dungeon.start_room; room= dungeon; visible=sight}
 
 let player_loc state= state.location
 
-let move state dir= 
-  curr= player_loc state
+let move coor dir= 
   match dir with
-  | "w" -> {(fst curr, snd curr + 1)}
-  | "a" -> {(fst curr - 1, snd curr)}
-  | "s" -> {(fst curr, snd curr - 1)}
-  | "d" -> {(fst curr + 1, snd curr)}
-  | _ -> state
+  | "w" -> {(fst coor, snd coor + 1)}
+  | "a" -> {(fst coor - 1, snd coor)}
+  | "s" -> {(fst coor, snd coor - 1)}
+  | "d" -> {(fst coor + 1, snd coor)}
+  | _ -> coor
