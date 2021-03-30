361f195f7edde2e07cd28cc197c41e732ee0e97d
Xiaolong Gao <xg54@cornell.edu>
2021-03-30 19:14:16 -0400

    state

--- state.ml
+++ state.ml
@@ -1,18 +1,26 @@
 type current = {
-  location : int * int;
-  room : Dungeon.t;
+  mutable location : int * int;
+  mutable room : Dungeon.t;
 }
 
 let init_state dungeon =
-  let current_loc = dungeon |> Dungeon.get_start in
-  { location = current_loc; room = dungeon }
+  { location = dungeon |> Dungeon.get_start; room = dungeon }
 
 let player_loc state = state.location
 
-let move coor dir =
-  match dir with
-  | "w" -> (fst coor, snd coor + 1)
-  | "a" -> (fst coor - 1, snd coor)
-  | "s" -> (fst coor, snd coor - 1)
-  | "d" -> (fst coor + 1, snd coor)
-  | _ -> coor
+let move current key =
+  let x, y = current.location in
+  match key with
+  | Glut.KEY_RIGHT ->
+      current.location <- (x + 1, y);
+      current
+  | Glut.KEY_LEFT ->
+      current.location <- (x - 1, y);
+      current
+  | Glut.KEY_UP ->
+      current.location <- (x, y + 1);
+      current
+  | Glut.KEY_DOWN ->
+      current.location <- (x, y - 1);
+      current
+  | _ -> current
--- state.mli
+++ state.mli
@@ -1,7 +1,7 @@
-open Dungeon
-
 type current
 
-val init_state: Dungeon.t -> current
+val init_state : Dungeon.t -> current
 
-val player_loc: current -> int*int
+val player_loc : current -> int * int
+
+val move : current -> Glut.special_key_t -> current
