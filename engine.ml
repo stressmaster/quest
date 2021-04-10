81269c89b869d3dbd1fd94e1ffaa1a0f9e1d5e32
Xiaolong Gao <xg54@cornell.edu>
2021-04-09 21:24:31 -0400

    limit breakerrrrrr

--- engine.ml
+++ engine.ml
@@ -1,25 +1,37 @@
-let init_engine textur_list w h x_length y_length =
-  let dungeon = Dungeon.instantiate_dungeon 20 30 7 in
-  let game = ref (State.init_state dungeon) in
+let init_texture texture_list =
   Bitmap.maximum_live := 15000000;
   Bitmap.maximum_block_size := !Bitmap.maximum_live / 16;
   let r = Gc.get () in
   r.Gc.max_overhead <- 30;
   Gc.set r;
+  Texturemap.init_texture texture_list
+
+let init_window w h =
   ignore (Glut.init Sys.argv);
   Glut.initDisplayMode ~alpha:true ~depth:true ();
   Glut.initWindowSize ~w ~h;
-  ignore (Glut.createWindow ~title:"CamelQuest");
-  Texturemap.init_texture texture_list;
+  ignore (Glut.createWindow ~title:"CamelQuest")
+
+let init_display dungeon game w h =
   Glut.displayFunc ~cb:(fun () ->
       GlClear.color (0.0, 0.0, 0.0);
       GlClear.clear [ `color ];
       GluMat.ortho2d ~x:(0.0, float_of_int w) ~y:(0.0, float_of_int h);
       GlMat.mode `projection;
       let g = !game in
-      render_dungeon (State.player_loc g) dungeon;
-      Gl.flush ());
+      Dungeon.render_dungeon (State.player_loc g) dungeon;
+      Gl.flush ())
+
+let init_input game =
   Glut.keyboardFunc ~cb:(fun ~key ~x ~y -> if key = 27 then exit 0);
-  Glut.specialFunc ~cb:(fun ~key ~x ~y -> game := State.move !game key);
+  Glut.specialFunc ~cb:(fun ~key ~x ~y -> game := State.move !game key)
+
+let init_engine texture_list w h x_length y_length =
+  let dungeon = Dungeon.instantiate_dungeon 20 30 7 in
+  let game = ref (State.init_state dungeon) in
+  init_texture texture_list;
+  init_window w h;
+  init_display dungeon game w h;
+  init_input game;
   Glut.idleFunc ~cb:(Some Glut.postRedisplay);
   Glut.mainLoop ()
--- main.ml
+++ main.ml
@@ -26,35 +26,7 @@ let height = float_of_int h /. float_of_int y_length
 
 let square_height ~height = float_of_int h /. height
 
-(*let dungeon = Dungeon.instantiate_dungeon 20 30 7
-
-  let game = ref (State.init_state dungeon)*)
-
-let _ =
-  Bitmap.maximum_live := 15000000;
-  Bitmap.maximum_block_size := !Bitmap.maximum_live / 16;
-  let r = Gc.get () in
-  r.Gc.max_overhead <- 30;
-  Gc.set r
-
 (* [main] renders the game*)
-let main () =
-  ignore (Glut.init Sys.argv);
-  Glut.initDisplayMode ~alpha:true ~depth:true ();
-  Glut.initWindowSize ~w ~h;
-  ignore (Glut.createWindow ~title:"CamelQuest");
-  Texturemap.init_texture texture_list;
-  Glut.displayFunc ~cb:(fun () ->
-      GlClear.color (0.0, 0.0, 0.0);
-      GlClear.clear [ `color ];
-      GluMat.ortho2d ~x:(0.0, float_of_int w) ~y:(0.0, float_of_int h);
-      GlMat.mode `projection;
-      (*let g = !game in*)
-      (*Dungeon.render_dungeon (State.player_loc g) dungeon;*)
-      Gl.flush ());
-  (*Glut.keyboardFunc ~cb:(fun ~key ~x ~y -> if key = 27 then exit 0);
-    Glut.specialFunc ~cb:(fun ~key ~x ~y -> game := State.move !game
-    key); Glut.idleFunc ~cb:(Some Glut.postRedisplay);*)
-  Glut.mainLoop ()
+let main () = Engine.init_engine texture_list w h x_length y_length
 
 let _ = main ()
