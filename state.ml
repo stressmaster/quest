0d7bc79783341ba0a1202d700e43675595379852
Xiaolong Gao <xg54@cornell.edu>
2021-04-16 18:32:52 -0400

    the fockers

--- state.ml
+++ state.ml
@@ -21,6 +21,7 @@ type current = {
   mutable room : Dungeon.t;
   mutable room_exit : int * int;
   mutable in_fight : bool;
+  mutable action : action;
 }
 
 let curr_room c = c.room
@@ -34,6 +35,7 @@ let init_state file_name =
     room_exit = r |> Dungeon.get_exit;
     location = r |> Dungeon.get_start;
     in_fight = false;
+    action = Run;
   }
 
 let fight_decision bound = Random.int bound = 0
@@ -47,46 +49,62 @@ let player_loc state = state.location
 let map_move_controller current key =
   let current_bound = Dungeon.get_bound current.room in
   let x, y = current.location in
-  ( match key with
+  match key with
   | Glut.KEY_RIGHT ->
       current.location <-
         ( if Dungeon.is_wall current.room (x + 1, y) then (x, y)
         else (x + 1, y) );
       current.in_fight <- fight_decision current_bound;
       (*debug stuff start*)
-      debug_encounters current (*debug stuff end*)
+      debug_encounters current (*debug stuff end*);
+      current
   | Glut.KEY_LEFT ->
       current.location <-
         ( if Dungeon.is_wall current.room (x - 1, y) then (x, y)
         else (x - 1, y) );
       current.in_fight <- fight_decision current_bound;
       (*debug stuff start*)
-      debug_encounters current (*debug stuff end*)
+      debug_encounters current (*debug stuff end*);
+      current
   | Glut.KEY_UP ->
       current.location <-
         ( if Dungeon.is_wall current.room (x, y + 1) then (x, y)
         else (x, y + 1) );
       current.in_fight <- fight_decision current_bound;
       (*debug stuff start*)
-      debug_encounters current (*debug stuff end*)
+      debug_encounters current (*debug stuff end*);
+      current
   | Glut.KEY_DOWN ->
       current.location <-
         ( if Dungeon.is_wall current.room (x, y - 1) then (x, y)
         else (x, y - 1) );
       current.in_fight <- fight_decision current_bound;
       (*debug stuff start*)
-      debug_encounters current (*debug stuff end*)
-  | _ -> () );
+      debug_encounters current (*debug stuff end*);
+      current
+  | _ -> current
 
-  let should_change_room = current.room_exit = current.location in
-  current.room <-
-    ( if should_change_room then
-      Game.next_dungeon current.game current.room
-    else current.room );
-  current.location <-
-    ( if should_change_room then Dungeon.get_start current.room
-    else current.location );
-  current.room_exit <-
-    ( if should_change_room then Dungeon.get_exit current.room
-    else current.room_exit );
-  current
+let menu_move current key =
+  match key with
+  | Glut.KEY_RIGHT ->
+      current.action <- get_next_action current.action;
+      current
+  | Glut.KEY_LEFT ->
+      current.action <- get_prev_action current.action;
+      current
+  | Glut.KEY_F2 ->
+      current.in_fight <- false;
+      current
+  | Glut.KEY_F1 ->
+      print_string
+        ( match current.action with
+        | Run -> "run"
+        | Attack -> "attack"
+        | Recover -> "recover" );
+      current
+  | _ -> current
+
+(* [controller current key] updates the [current] based on [key]*)
+let controller current key =
+  if current.in_fight then menu_move current key
+  else map_move_controller current key
