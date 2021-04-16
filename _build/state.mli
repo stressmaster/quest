cb61cf59fa126b37763fb88605ca730e019a1fc3
Daniel Jann <dj293@cornell.edu>
2021-04-16 19:08:46 -0400

    fuwu

--- _build/state.mli
+++ _build/state.mli
@@ -7,7 +7,7 @@ val init_state : string -> current
 (* [player_loc] returns cords of current player location *)
 val player_loc : current -> int * int
 
-(* [move] updates player location *)
-val map_move_controller : current -> Glut.special_key_t -> current
+(* [controller] updates current based on key input *)
+val controller : current -> Glut.special_key_t -> current
 
 val curr_room : current -> Dungeon.t
