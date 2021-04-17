98e9461060f32442f244ed78d60c1b37819924a5
Xiaolong Gao <xg54@cornell.edu>
2021-04-16 20:58:22 -0400

    the rebuilding age

--- texturemap.mli
+++ texturemap.mli
@@ -1,8 +1,11 @@
 (* [init_texture] initializes texture *)
 val init_texture : string list -> unit
 
+(* [get_texture texture_name] returns texture*)
+val get_texture : string -> 'a
+
 (* [set_texture] sets texture *)
-val set_texture : string -> unit
+val set_texture : 'a -> unit
 
 (* [start_texture] enables texture *)
 val start_texture : unit -> unit
