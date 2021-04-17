3c7c34171a7132bb05ca8c60a67f09e309e0bb74
Daniel Jann <dj293@cornell.edu>
2021-04-16 22:09:43 -0400

    uwu

--- fight_menu.ml
+++ fight_menu.ml
@@ -14,18 +14,18 @@ let render_menu
   | Run ->
       Render.render_square
         (Render.new_square
-           (float_of_int 2 /. float_of_int 3 *. 2.)
+           (10. /. float_of_int Magic_numbers.x_length *. 2.)
            (0.5 /. float_of_int 3 *. 2.)
            Magic_numbers.width Magic_numbers.height "./path.png")
   | Recover ->
       Render.render_square
         (Render.new_square
-           (float_of_int 1 /. float_of_int 3 *. 2.)
+           (5. /. float_of_int Magic_numbers.x_length *. 2.)
            (0.5 /. float_of_int 3 *. 2.)
            Magic_numbers.width Magic_numbers.height "./wall.png")
   | Attack ->
       Render.render_square
         (Render.new_square
-           (float_of_int 0 /. float_of_int 3 *. 2.)
+           (0. /. float_of_int Magic_numbers.x_length *. 2.)
            (0.5 /. float_of_int 3 *. 2.)
            Magic_numbers.width Magic_numbers.height "./player.png")
