e725ded989cd9ab1ad637c8a49c3ecc1095e07f1
John Lo <jdl283@cornell.edu>
2021-04-16 20:22:28 -0400

    improved fight menu

--- fight_menu.ml
+++ fight_menu.ml
@@ -5,18 +5,18 @@ let render_menu (a : action) =
   | Run ->
       Render.render_square
         (Render.new_square
-           (float_of_int 0 /. float_of_int 3 *. 2.)
-           (float_of_int 0 /. float_of_int 3 *. 2.)
-           Magic_numbers.width Magic_numbers.height "./player.png")
+           (float_of_int 2 /. float_of_int 3 *. 2.)
+           (0.5 /. float_of_int 3 *. 2.)
+           Magic_numbers.width Magic_numbers.height "./path.png")
   | Recover ->
       Render.render_square
         (Render.new_square
            (float_of_int 1 /. float_of_int 3 *. 2.)
-           (float_of_int 1 /. float_of_int 3 *. 2.)
-           Magic_numbers.width Magic_numbers.height "./player.png")
+           (0.5 /. float_of_int 3 *. 2.)
+           Magic_numbers.width Magic_numbers.height "./wall.png")
   | Attack ->
       Render.render_square
         (Render.new_square
-           (float_of_int 2 /. float_of_int 3 *. 2.)
-           (float_of_int 2 /. float_of_int 3 *. 2.)
+           (float_of_int 0 /. float_of_int 3 *. 2.)
+           (0.5 /. float_of_int 3 *. 2.)
            Magic_numbers.width Magic_numbers.height "./player.png")
