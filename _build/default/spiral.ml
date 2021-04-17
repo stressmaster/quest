2a2ad1efdda8d45560829790f70fd5a88b1936c4
John Lo <jdl283@cornell.edu>
2021-04-17 17:11:12 -0400

    spiral does not crash (but does not work as intended)

--- _build/default/spiral.ml
+++ _build/default/spiral.ml
@@ -4,35 +4,39 @@ type direction =
   | Right
   | Left
 
+let noexn_hashtable_find table elt =
+  match try Some (Hashtbl.find table elt) with Not_found -> None with
+  | Some a -> true
+  | None -> false
+
 let rec turner (fight : State.fight) cur_x cur_y dir end_x end_y table =
-  Render.render_square
-    (Render.new_square
-       (float_of_int (cur_x / Magic_numbers.x_length * 2))
-       (float_of_int (cur_y / Magic_numbers.y_length * 2))
-       Magic_numbers.width Magic_numbers.height "./path.png");
+  (*Render.render_square (Render.new_square (float_of_int (cur_x /
+    Magic_numbers.x_length * 2)) (float_of_int (cur_y /
+    Magic_numbers.y_length * 2)) Magic_numbers.width
+    Magic_numbers.height "./path.png");*)
   Hashtbl.add table (cur_x, cur_y) true;
   if cur_x = end_x && cur_y = end_y then fight.spiraled <- true
   else
     (* pause for a little while*)
-    let delay = Sys.time () +. 0.001 in
+    let delay = Sys.time () +. 0.01 in
     let rec wait d =
       if Sys.time () > d then
         match dir with
         | Right ->
-            if Hashtbl.find table (cur_x, cur_y - 1) = false then
-              turner fight cur_x (cur_y - 1) Down end_x end_y table
+            if noexn_hashtable_find table (cur_x, cur_y - 1) = false
+            then turner fight cur_x (cur_y - 1) Down end_x end_y table
             else turner fight (cur_x + 1) cur_y Right end_x end_y table
         | Down ->
-            if Hashtbl.find table (cur_x - 1, cur_y) = false then
-              turner fight (cur_x - 1) cur_y Left end_x end_y table
+            if noexn_hashtable_find table (cur_x - 1, cur_y) = false
+            then turner fight (cur_x - 1) cur_y Left end_x end_y table
             else turner fight cur_x (cur_y - 1) Down end_x end_y table
         | Left ->
-            if Hashtbl.find table (cur_x, cur_y + 1) = false then
-              turner fight cur_x (cur_y + 1) Up end_x end_y table
+            if noexn_hashtable_find table (cur_x, cur_y + 1) = false
+            then turner fight cur_x (cur_y + 1) Up end_x end_y table
             else turner fight (cur_x - 1) cur_y Left end_x end_y table
         | Up ->
-            if Hashtbl.find table (cur_x + 1, cur_y) = false then
-              turner fight (cur_x + 1) cur_y Right end_x end_y table
+            if noexn_hashtable_find table (cur_x + 1, cur_y) = false
+            then turner fight (cur_x + 1) cur_y Right end_x end_y table
             else turner fight cur_x (cur_y + 1) Up end_x end_y table
       else wait d
     in
