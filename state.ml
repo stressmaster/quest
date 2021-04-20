dee9997cd9cb73a05f176f506952d2e24446a408
Daniel Jann <dj293@cornell.edu>
2021-04-20 19:29:01 -0400

    fixed damage

--- state.ml
+++ state.ml
@@ -79,20 +79,20 @@ let map_move current key =
     match key with
     | Glut.KEY_RIGHT ->
         current.location <-
-          ( if Dungeon.is_wall current.room (x + 1, y) then (x, y)
-          else (x + 1, y) )
+          (if Dungeon.is_wall current.room (x + 1, y) then (x, y)
+          else (x + 1, y))
     | Glut.KEY_LEFT ->
         current.location <-
-          ( if Dungeon.is_wall current.room (x - 1, y) then (x, y)
-          else (x - 1, y) )
+          (if Dungeon.is_wall current.room (x - 1, y) then (x, y)
+          else (x - 1, y))
     | Glut.KEY_UP ->
         current.location <-
-          ( if Dungeon.is_wall current.room (x, y + 1) then (x, y)
-          else (x, y + 1) )
+          (if Dungeon.is_wall current.room (x, y + 1) then (x, y)
+          else (x, y + 1))
     | Glut.KEY_DOWN ->
         current.location <-
-          ( if Dungeon.is_wall current.room (x, y - 1) then (x, y)
-          else (x, y - 1) )
+          (if Dungeon.is_wall current.room (x, y - 1) then (x, y)
+          else (x, y - 1))
     | _ -> ()
   end;
   let should_change_room = current.room_exit = current.location in
@@ -100,7 +100,7 @@ let map_move current key =
   if should_change_room then (
     current.room <- Game.next_dungeon current.game current.room;
     current.location <- Dungeon.get_start current.room;
-    current.room_exit <- Dungeon.get_exit current.room );
+    current.room_exit <- Dungeon.get_exit current.room);
   current
 
 let typing_case current key =
@@ -109,20 +109,19 @@ let typing_case current key =
   and mon_HP = current.fight.monster_health in
   if key = 13 then (
     let diff = Levenshtein.dist str mon_str in
-    ( match current.fight.action with
+    (match current.fight.action with
     | Attack ->
-        current.fight.monster_health <-
-          max (mon_HP - String.length mon_str + diff) 0
+        let damage = max (String.length mon_str - diff) 0 in
+        current.fight.monster_health <- max (mon_HP - damage) 0
     | Recover ->
         current.health <-
           min
             (current.fight.player_health + String.length mon_str - diff)
             current.health
     | Run ->
-        if diff <= String.length str / 3 then current.in_fight <- false
-    );
+        if diff <= String.length str / 3 then current.in_fight <- false);
     current.fight.attacking <- false;
-    "" )
+    "")
   else if key = 127 then
     String.sub str 0 (max (String.length str - 1) 0)
   else str ^ Char.escaped (Char.chr key)
@@ -135,7 +134,7 @@ let typing_move current key =
   current
 
 let menu_move current key =
-  ( match key with
+  (match key with
   | Glut.KEY_RIGHT ->
       current.fight.action <- get_next_action current.fight.action
   | Glut.KEY_LEFT ->
@@ -143,7 +142,7 @@ let menu_move current key =
   | Glut.KEY_DOWN -> current.in_fight <- false
   | Glut.KEY_UP ->
       current.fight.attacking <- not current.fight.attacking
-  | _ -> () );
+  | _ -> ());
   current
 
 (* [controller current key] updates the [current] based on [key]*)
@@ -152,7 +151,7 @@ let controller current key =
     current.fight.monster_health <-
       Dungeon.get_monster_HP current.fight.monster;
     current.in_fight <- false;
-    current )
+    current)
   else if current.in_fight && not current.fight.attacking then
     menu_move current key
   else if not current.in_fight then map_move current key
