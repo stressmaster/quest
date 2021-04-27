afaf50edcf2d45d6f59a2a7432c65e86fd044e75
Xiaolong Gao <xg54@cornell.edu>
2021-04-27 17:51:06 -0400

    timer no longer ramiel

--- fight_menu.ml
+++ fight_menu.ml
@@ -24,7 +24,7 @@ let render_menu (fight : State.fight) =
            ( 500.
            *. (Timer.current_time () |> float_of_int)
            /. (fight.typing_limit |> float_of_int) )
-           Magic_numbers.height fight.monster.sprite);
+           Magic_numbers.height "./timer.png");
       Font.render_font
         (Font.new_font fight.monster_string 0. 0.3 Magic_numbers.width
            Magic_numbers.height) );
