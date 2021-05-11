269e1f142cf3a4f16842e6e1fb3f5484f941b979
Matthew Rosen <mhr79@cornell.edu>
2021-05-11 17:13:54 -0400

    the experiment is over

--- engine.ml
+++ engine.ml
@@ -15,21 +15,17 @@ let stack_push x = Stack.push x stack
 
 let stack_pop () = Stack.pop stack
 
-let dim = ref (Magic_numbers.w, Magic_numbers.h)
-
 let init_window w h =
   ignore (Glut.init Sys.argv);
   Glut.initDisplayMode ~alpha:true ~depth:true ();
   Glut.initWindowSize ~w ~h;
   ignore (Glut.createWindow ~title:"CamelQuest")
 
-let init_display game =
+let init_display game w h =
   Glut.displayFunc ~cb:(fun () ->
-      let w, h = !dim in
-      GluMat.ortho2d ~x:(0.0, float_of_int w) ~y:(0.0, float_of_int h);
-      let c = min w h in
-      GlDraw.viewport (w - c) (h - c) (2 * c) (2 * c);
       GlClear.color (0.0, 0.0, 0.0);
+      GlClear.clear [ `color ];
+      GluMat.ortho2d ~x:(0.0, float_of_int w) ~y:(0.0, float_of_int h);
       GlMat.mode `projection;
       ( match Render_stack.stack_peek () with
       | SpiralRender ->
@@ -61,15 +57,13 @@ let init_input game =
   Glut.specialFunc ~cb:(fun ~key ~x ~y ->
       game := State.controller !game key)
 
-let reshape ~w ~h = dim := (w, h)
-
 let init_engine texture_list w h x_length y_length =
   let game = ref (State.init_state "sample_game.json") in
   let start = Sys.time () in
   init_texture texture_list;
   init_audio ();
   init_window w h;
-  init_display game;
+  init_display game w h;
   init_input game;
   let rec timer ~value =
     Timer.increase_time 1;
@@ -84,5 +78,4 @@ let init_engine texture_list w h x_length y_length =
   (*Glut.idleFunc ~cb:(Some Glut.postRedisplay);*)
   Glut.timerFunc ~ms ~cb:timer ~value:ms;
   Glut.timerFunc ~ms ~cb:typing_timer ~value:ms;
-  Glut.reshapeFunc reshape;
   Glut.mainLoop ()
