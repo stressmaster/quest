f78c0c3e6648ec365e930c09feec8f099c1839d1
Matthew Rosen <mhr79@cornell.edu>
2021-05-22 15:55:37 -0400

    authors'

---
+++ authors.mli
@@ -0,0 +1,37 @@
+(************************************************************ 
+   Copyright (C) 2021 Cornell University.
+   Created by Michael Clarkson (mrc26@cornell.edu) and CS 3110 course staff.
+   You may not redistribute this assignment, distribute any derivatives,
+   or use it for commercial purposes.
+ ************************************************************)
+
+(** CS 3110 Spring 2021 Assignment Final Project
+
+    @author Xialong Gao Matthew Rosen Daniel Jann John Lo *)
+
+(* TODO: complete the academic integrity statement below. *)
+
+(************************************************************ 
+   Academic Integrity Statement
+   We, the people named in the @author comment above, have fully reviewed the
+   course academic integrity policies. We have adhered to those policies in
+   solving the assignment.
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If we did engage in such collaboration, here is the
+   list of other students with whom we collaborated, and a brief summary of that
+   collaboration:
+   - none
+   Here are any deviations from the policies that we want to document, with the
+   understanding that being honest about our mistakes is more honorable than
+   lying about our intellectual work:
+   - none
+   We acknowledge that the partner collaboration policy requires all work to be
+   done together. Both partners are equally responsible for the entire
+   submission. It is against that policy to split up the work. But if we did
+   violate that policy, here is how we split up the work:
+   - N/A
+ ************************************************************)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
