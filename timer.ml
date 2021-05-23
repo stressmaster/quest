45fcffe3e6822e04c24803c7f611089d46d86a83
Xiaolong Gao <xg54@cornell.edu>
2021-05-22 22:57:57 -0400

    timing

--- timer.ml
+++ timer.ml
@@ -1,7 +1,30 @@
-let time = ref 0
+type id = string
 
-let current_time () = !time
+type time = {
+  increment : int;
+  ticker : int ref;
+}
 
-let reset_timer () = time := 0
+let timers = ref []
 
-let increase_time t = time := !time + t
+let init_timers (id_increment_list : (id * int) list) =
+  let add_to_timers h =
+    timers := (fst h, { increment = snd h; ticker = ref 0 }) :: !timers
+  in
+  List.iter add_to_timers id_increment_list
+
+let current_time id =
+  let { increment; ticker } = List.assoc id !timers in
+  ticker
+
+let reset_timer id =
+  let { increment; ticker } = List.assoc id !timers in
+  ticker := 0
+
+let step_timer () =
+  let step_ticker h =
+    let id, { increment; ticker } = h in
+    ticker := !ticker + increment;
+    h
+  in
+  timers := List.map step_ticker !timers
--- timer.mli
+++ timer.mli
@@ -1,7 +1,14 @@
+type id = string
+
+type time
+
+val init_timers : (id * int) list -> unit
+
 (* [current_time] shows the current distance from start *)
-val current_time : unit -> int
+val current_time : id -> int
 
 (* [reset_timer] resets the timer *)
-val reset_timer : unit -> unit
+val reset_timer : id -> unit
 
-val increase_time : int -> unit
+(* [step_timer] steps each one of the timer *)
+val step_timer : unit -> unit
