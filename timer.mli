24827b750ae1ac6fe5743820e2b8a3dd7cba7b60
Daniel Jann <dj293@cornell.edu>
2021-05-24 15:19:56 -0400

    push

--- timer.mli
+++ timer.mli
@@ -1,14 +1,26 @@
+(**Representation of timer data.
+
+   This module contains the mutable data used to keep track of time
+   elapsed within the game.*)
+
+(**The strings by which we identify each timer*)
 type id = string
 
+(**The abstract*)
 type time
 
+(**[init_timers \[(id1, i1), (id2, i2), ... (idk, ik)\]] initializes
+   timers [id1 ... idk] with increments [i1 ... ik]. Each timer will
+   then increment by that amount each time in step_timer ().*)
 val init_timers : (id * int) list -> unit
 
-(* [current_time] shows the current distance from start *)
+(** [current_time id] shows the current amount of time recorded by timer
+    (id). *)
 val current_time : id -> int
 
-(* [reset_timer] resets the timer *)
+(* [reset_timer id] resets the timer (id)*)
 val reset_timer : id -> unit
 
-(* [step_timer] steps each one of the timer *)
+(* [step_timer ()] steps each one of the timers according to their
+   increment value*)
 val step_timer : unit -> unit
--- win_menu.mli
+++ win_menu.mli
@@ -1,1 +1,5 @@
+(**This module renders the menu that appears upon winning the game*)
+
+(**[render_menu start finish] renders the menu based on (start) and
+   (finish)*)
 val render_menu : int -> int -> unit
