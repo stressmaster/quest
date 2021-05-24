6802b5cccb9c7ccb1acfe961d1ec2bd832621442
Daniel Jann <dj293@cornell.edu>
2021-05-24 16:54:36 -0400

    doc

--- audio.mli
+++ audio.mli
@@ -2,20 +2,20 @@
 
     This module plays music and includes functions for changing music. *)
 
-(* abstract type representing audio *)
+(** abstract type representing audio *)
 type t
 
-(* [set_music music] initializes the music to [music] *)
+(** [set_music music] initializes the music to [music] *)
 val set_music : string -> unit
 
-(* [play_music ()] plays the current music instance *)
+(** [play_music ()] plays the current music instance *)
 val play_music : unit -> unit
 
-(* [halt_music ()] halts the current music instance *)
+(** [halt_music ()] halts the current music instance *)
 val halt_music : unit -> unit
 
-(* [change_music ()] changes the music to [music] *)
+(** [change_music ()] changes the music to [music] *)
 val change_music : string -> unit
 
-(* [play_sound sound] plays [sound] *)
+(** [play_sound sound] plays [sound] *)
 val play_sound : string -> unit
