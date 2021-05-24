e82161ce179be79b75358d9f66b972781453d97f
Daniel Jann <dj293@cornell.edu>
2021-05-24 15:33:39 -0400

    doc

--- dungeon.mli
+++ dungeon.mli
@@ -4,69 +4,72 @@
     dungeon, including a data about each tile. Includes methods for
     obtaining information about the dungeon. *)
 
-(* an abstract type representing a dungeon*)
+(** an abstract type representing a dungeon*)
 type t
 
-(* an abstract type representing a tile*)
+(** an abstract type representing a tile*)
 type tile
 
-(* an abstract type representing a dungeon cell*)
+(** an abstract type representing a dungeon cell*)
 type cell
 
-(* [instantiate_dungeon x y] is a dungeon with [x] columns [y] rows *)
+(** [instantiate_dungeon x y] is a dungeon with [x] columns [y] rows *)
 val instantiate_dungeon :
   ?seed:int -> int -> int -> int -> int * int -> int -> int -> int -> t
 
-(* [is_wall dungeon (x, y)] is true iff the cell at coordinate [(x, y)]
-   is a wall in [dungeon] *)
+(** [is_wall dungeon (x, y)] is true iff the cell at coordinate [(x, y)]
+    is a wall in [dungeon] *)
 val is_wall : t -> int * int -> bool
 
+(**[get_item dungeon (x,y)] returns the Item.t option associated with
+   tile (x,y), and then removes the item from that tile*)
 val get_item : t -> int * int -> Item.t option
 
+(**[drop_item dungeon (x,y) item] places (item) on the tile (x,y)*)
 val drop_item : t -> int * int -> Item.t option -> unit
 
-(* [is_wall dungeon (x, y)] is the material of the tile at [(x,y)] in
-   [dungeon] *)
+(** [is_wall dungeon (x, y)] is the material of the tile at [(x,y)] in
+    [dungeon] *)
 val tile_material : tile -> string
 
-(* [get_start dungeon] is the coordinates of the starting tile in
-   [dungeon]*)
+(** [get_start dungeon] is the coordinates of the starting tile in
+    [dungeon]*)
 val get_start : t -> int * int
 
-(* [get_exit dungeon] is the coordinates of the exit tile in [dungeon]*)
+(** [get_exit dungeon] is the coordinates of the exit tile in [dungeon]*)
 val get_exit : t -> int * int
 
-(* [get_dimensions dungeon] is the pair of ints representing the
-   dimensions of [dungeon]*)
+(** [get_dimensions dungeon] is the pair of ints representing the
+    dimensions of [dungeon]*)
 val get_dimensions : t -> int * int
 
-(* [get_cells dungeon] is the hash table representing the cells in
-   [dungeon]*)
+(** [get_cells dungeon] is the hash table representing the cells in
+    [dungeon]*)
 val get_cells : t -> (int * int, cell) Hashtbl.t
 
-(* [get_cells cell] is the tile associated with [cell] *)
+(** [get_cells cell] is the tile associated with [cell] *)
 val get_tile : cell -> tile
 
-(* [get_bound dungeon] is the bound of [dungeon]*)
+(** [get_bound dungeon] is the bound of [dungeon]*)
 val get_bound : t -> int
 
-(* [get_id dungeon] is the id of [dungeon]*)
+(** [get_id dungeon] is the id of [dungeon]*)
 val get_id : t -> int
 
-(* [get_next dungeon] is the id of the dungeon that follows [dungeon]*)
+(** [get_next dungeon] is the id of the dungeon that follows [dungeon]*)
 val get_next : t -> int
 
-(* [get_prev dungeon] is the id of the dungeon that precedes [dungeon]*)
+(** [get_prev dungeon] is the id of the dungeon that precedes [dungeon]*)
 val get_prev : t -> int
 
-(* [get_time dungeon] is the time seed associated with [dungeon] *)
+(** [get_time dungeon] is the time seed associated with [dungeon] *)
 val get_time : t -> int
 
-(* [render_dungeon (x,y) dungeon condition] renders [dungeon] with
-   dungeon coordinates [(x,y)] at the center of the window and with NPCs
-   if [condition] *)
+(** [render_dungeon (x,y) dungeon condition] renders [dungeon] with
+    dungeon coordinates [(x,y)] at the center of the window and with
+    NPCs if [condition] *)
 val render_dungeon : int * int -> t -> bool -> unit
 
-(* [get_magic_numbers dungeon] is the magic_numbers associated with
-   [dungeon] *)
+(** [get_magic_numbers dungeon] is the magic_numbers associated with
+    [dungeon] *)
 val get_magic_numbers : t -> Magic_numbers.t
