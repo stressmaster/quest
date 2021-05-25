38f7be9cfcc86bdbf38771956306cb496d1f08b5
Xiaolong Gao <xg54@cornell.edu>
2021-05-24 22:19:01 -0400

    clarkosn

--- win_menu.ml
+++ win_menu.ml
@@ -1,6 +1,6 @@
 let render_menu () =
+  Render.render_square
+    (Render.new_square 0. 0. 500. 500. "./clarkson.png");
   Font.render_font
     (Font.new_font "Congratulations!" 0.2 0. Magic_numbers.width
-       Magic_numbers.height);
-  Render.render_square
-    (Render.new_square 0. 0. 500. 500. "./clarkson.png")
+       Magic_numbers.height)
