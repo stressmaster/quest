84394cd2df6f73401ca71eae863823c9a468cceb
Xiaolong Gao <xg54@cornell.edu>
2021-05-24 23:34:39 -0400

    fuck

--- render_stack.ml
+++ render_stack.ml
@@ -10,35 +10,11 @@ type scene =
 
 let stack = Stack.create ()
 
-let string_of_scene a =
-  match a with
-  | DungeonRender -> "DungeonRender"
-  | FightRender -> "FightRender"
-  | SpiralRender -> "SpiralRender"
-  | AttackRender -> "AttackRender"
-  | ScreenshakeRender -> "ScreenshakeRender"
-  | WinRender -> "WinRender"
-  | GameoverRender -> "GameoverRender"
-  | StartRender -> "StartRender"
-
-let debug_stack r p =
-  match (r, p) with
-  | r, true -> ">>>> PUSH " ^ string_of_scene r
-  | r, false -> "<<< POP " ^ string_of_scene r
-
 let stack_peek () = Stack.top stack
 
-let stack_push x =
-  print_string (debug_stack x true);
-  print_newline ();
-  Stack.push x stack
+let stack_push x = Stack.push x stack
 
-let stack_pop () =
-  print_string (debug_stack (stack_peek ()) false);
-  print_newline ();
-  ignore (Stack.pop stack);
-  print_string (string_of_scene (stack_peek ()) ^ " is now top");
-  print_newline ()
+let stack_pop () = ignore (Stack.pop stack)
 
 let _ =
   stack_push DungeonRender;
